a7c1416bba1b8538cfc8e7d567bd85d67509fd61
BufonS <Curso Maana@DESKTOP-0LVMFH7>
2024-06-03 13:21:30 +0200

    ejercicio 4

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import com.example.Empleado.Dpt;
+import com.example.Persona.Genero;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,46 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      
+    	
+    	Empleado[] empleados = {
+    		Empleado.builder()
+    		.nombre("Adrián")
+    		.primerApellido("Santos")
+    		.segundoApellido("Santos")
+    		.genero(Genero.MASCULINO)
+    		.departamento(Dpt.INFORMATICA)
+    		.salario(2500)
+    		.build(),
+    		Empleado.builder()
+    		.nombre("Ana")
+    		.primerApellido("Celia")
+    		.segundoApellido("de Armas")
+    		.genero(Genero.FEMENINO)
+    		.departamento(Dpt.RRHH)
+    		.salario(3000)
+    		.build(),
+    		Empleado.builder()
+    		.nombre("Pedro")
+    		.primerApellido("Abascal")
+    		.segundoApellido("Fernandez")
+    		.genero(Genero.MASCULINO)
+    		.departamento(Dpt.LIMPIEZA)
+    		.salario(1500)
+    		.build(),
+    		Empleado.builder()
+    		.nombre("Anya")
+    		.primerApellido("Taylor")
+    		.segundoApellido("Joy")
+    		.genero(Genero.FEMENINO)
+    		.salario(2800)
+    		.build()
+    	};
+    	
+    	for(Empleado empleado : empleados)
+    		System.out.println(empleado.getSalario());
+   
+    	
+    	
     }
 }
